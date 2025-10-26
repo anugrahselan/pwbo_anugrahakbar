c40fa2fdaa51eac4c7f68a8f2f9bcb43aacc6b53
Anugrah0603 <akbarakbar0319@gmail.com>
2025-10-27 00:15:57 +0700

    add header

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -8,21 +8,22 @@
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar navbar-dark bg-primary">
-        <div class= "container">
-        <a class="navbar-brand" href="<?php echo BASEURL; ?>">PWBO</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-                <a class="nav-item nav-link active" href="<?php echo BASEURL; ?>">Home<span class="sr-only">(current)</span></a>
-                <a class="nav-item nav-link" href="<?php echo BASEURL; ?>/mahasiswa">Mahasiswa</a>
-                <a class="nav-item nav-link" href="<?php echo BASEURL; ?>/matakuliah">Matakuliah</a>
-                <a class="nav-item nav-link" href="<?php echo BASEURL; ?>/about">About</a>
+        <div class="container">
+            <a class="navbar-brand" href="<?php echo BASEURL; ?>">PWBO</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-item nav-link" href="<?php echo BASEURL; ?>">Home<span
+                            class="sr-only">(current)</span></a>
+                    <a class="nav-item nav-link" href="<?php echo BASEURL; ?>/mahasiswa">Mahasiswa</a>
+                    <a class="nav-item nav-link" href="<?php echo BASEURL; ?>/matakuliah">Matakuliah</a>
+                    <a class="nav-item nav-link" href="<?php echo BASEURL; ?>/about">About</a>
+                </div>
             </div>
-        </div>
         </div>
     </nav>
 </body>
 </html>
-
